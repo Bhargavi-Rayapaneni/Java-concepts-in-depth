bc9c3033dcdc5d647f859d7a2e4716eb236ea97b
Bhargavi <rayapanenibhargavi99@gmail.com>
2024-10-14 23:05:26 +0530

    Data Types

--- JavaPracticeCodes/Datatypes.java
+++ JavaPracticeCodes/Datatypes.java
@@ -2,6 +2,31 @@ package JavaPracticeCodes;
 
 public class Datatypes {
 
-    
+    public static void main(String[] args) {
+
+        /* Integer which is byte, short,int,long */
+
+        int num = 5; //size of int is 4 bytes
+        long n = 10; //size of long is 8 bytes
+        short s = 1; //size of short is 2 bytes
+        byte b = 8; //size of byte is 1 byte = 8 bits
+
+        /* Float which is double and float */
+        float f = 0.5f; //f must be add for float after .
+        double d = 14.8;
+
+        /* Character which is char */
+        char c = 'a'; //size of char is 2 bytes
+
+        /*Boolean type is accepting only true/false */
+
+        boolean correct = true;
+        boolean wrong = false;
+
+        System.out.println(correct);
+        System.out.println(f); //0.5
+        System.out.println(c);
+        System.out.println(d); //14.8
+    }
     
 }
