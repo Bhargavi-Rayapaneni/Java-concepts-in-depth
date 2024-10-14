0b9018e4af7f80c72a721d2ba449ba3d6e586df1
Bhargavi <rayapanenibhargavi99@gmail.com>
2024-10-14 22:51:25 +0530

    Variables

---
+++ JavaPracticeCodes/Datatypes.java
@@ -0,0 +1,7 @@
+package JavaPracticeCodes;
+
+public class Datatypes {
+
+    
+    
+}
---
+++ JavaPracticeCodes/Variables.java
@@ -0,0 +1,12 @@
+package JavaPracticeCodes;
+
+public class Variables {
+    public static void main(String[] args) {
+        int num = 5;
+        int num1 = 6;
+        String st = "Bhargavi";
+        System.out.println(num1 + num+st); //11Bhargavi
+    }
+   
+          
+}
